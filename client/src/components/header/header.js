69ee7de33d768c472ca27f7f35e738c17b48b208
Todd Tracy <trvcytg@gmail.com>
2019-12-18 20:21:32 -0600

    Fixed class declaration for JSX

--- client/src/components/header/header.js
+++ client/src/components/header/header.js
@@ -2,11 +2,11 @@ import React from "react";
 
 function Header(props) {
   return (
-    <section class="hero is-info is-bold">
-      <div class="hero-body">
-        <div class="container">
-          <h1 class="title">Header Title</h1>
-          <h2 class="subtitle">Header subtitle</h2>
+    <section className="hero is-info is-bold">
+      <div className="hero-body">
+        <div className="container">
+          <h1 className="title">Header Title</h1>
+          <h2 className="subtitle">Header subtitle</h2>
         </div>
       </div>
     </section>
