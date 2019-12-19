844f0fb624953b63fa8f1ad726d2da651ff14d6f
Todd Tracy <trvcytg@gmail.com>
2019-12-18 20:12:55 -0600

    Added initial JSX for Header component

---
+++ client/src/components/header/header.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Header(props) {
+  return (
+    <section class="hero is-info is-bold">
+      <div class="hero-body">
+        <div class="container">
+          <h1 class="title">Header Title</h1>
+          <h2 class="subtitle">Header subtitle</h2>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Header;
