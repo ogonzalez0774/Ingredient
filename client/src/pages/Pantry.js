e39c96463c573d4e13e7cf5b9adfe792cb1b3225
Sean Lim <lim_s@outlook.com>
2019-12-20 02:17:11 -0600

    commented out password and unit from user model

--- client/src/pages/Pantry.js
+++ client/src/pages/Pantry.js
@@ -1,62 +1,62 @@
 import React from "react";
 import API from "../utils/API";
-import PantryItem from "../components/PantryItem";
+import PantryItem from "../components/pantryItem";
 
 class Pantry extends React.Component {
-    state = {
-        userId: "5dfafab3a612c2884b4bd0bd",
-        ingredients: [
-            {
-                name: "chicken",
-                amount: 0
-            },
-            {
-                name: "bacon",
-                amount: 0
-            },
-            {
-                name: "ranch",
-                amount: 0
-            },
-            {
-                name: "lettuce",
-                amount: 0
-            }
-        ]
-    };
+  state = {
+    userId: "5dfafab3a612c2884b4bd0bd",
+    ingredients: [
+      {
+        name: "chicken",
+        amount: 0
+      },
+      {
+        name: "bacon",
+        amount: 0
+      },
+      {
+        name: "ranch",
+        amount: 0
+      },
+      {
+        name: "lettuce",
+        amount: 0
+      }
+    ]
+  };
 
-    componentDidMount() {
-        this.loadPantry();
+  componentDidMount() {
+    this.loadPantry();
+  }
+
+  loadPantry() {
+    API.getUser(this.state.userId).then(user => {
+      this.setState({ ingredients: user.data.ingredients });
+    });
+  }
+
+  togglePantryItem(userId, ingredientName) {
+    const newIngredients = this.state.ingredients;
+    for (const ingredient of newIngredients) {
+      if (ingredient.name === ingredientName) {
+        ingredient.amount = (ingredient.amount + 1) % 2;
+      }
     }
+    API.updateUser(userId, { ingredients: newIngredients });
+    this.loadPantry();
+  }
 
-    loadPantry() {
-        API.getUser(this.state.userId).then(user => {
-            this.setState({ ingredients: user.data.ingredients });
-        });
-    }
-
-    togglePantryItem(userId, ingredientName) {
-        const newIngredients = this.state.ingredients;
-        for (const ingredient of newIngredients) {
-            if (ingredient.name === ingredientName) {
-                ingredient.amount = (ingredient.amount + 1) % 2;
-            }
+  render() {
+    return this.state.ingredients.map(ingredient => (
+      <PantryItem
+        name={ingredient.name}
+        amount={ingredient.amount}
+        onClick={() =>
+          this.togglePantryItem(this.state.userId, ingredient.name)
         }
-        API.updateUser(userId, { ingredients: newIngredients });
-        this.loadPantry();
-    }
-
-    render() {
-        return this.state.ingredients.map(ingredient => (
-            <PantryItem
-                name={ingredient.name}
-                amount={ingredient.amount}
-                onClick={() =>
-                    this.togglePantryItem(this.state.userId, ingredient.name)
-                }
-            />
-        ));
-    }
+      />
+    ));
+  }
 }
 
 export default Pantry;
