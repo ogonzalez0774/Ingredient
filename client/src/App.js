ba6ae6c3c588a7f4c3705bbeabba633ff3a5794a
Sean Lim <lim_s@outlook.com>
2019-12-20 06:35:06 -0600

    container as page,  renders shoplist

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,21 @@ import "./App.css";
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Pantry from "./pages/Pantry";
+import Shoplist from "./pages/Shoplist";
+import Container from "./pages/Container";
 
 class App extends React.Component {
-    render() {
-        return <Pantry />;
-    }
+  render() {
+    return (
+      <>
+        {/* <Container> */}
+        <Pantry />
+        <Shoplist />
+
+        {/* </Container> */}
+      </>
+    );
+  }
 }
 
 export default App;
