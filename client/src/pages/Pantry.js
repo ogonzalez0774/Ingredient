62d316adc0fb9846ba32831f517d13636ad075a9
Todd Tracy <trvcytg@gmail.com>
2019-12-21 12:17:18 -0600

    slight change to capitalizaiton of PantryItem link

--- client/src/pages/Pantry.js
+++ client/src/pages/Pantry.js
@@ -1,47 +1,44 @@
 import React from "react";
 import API from "../utils/API";
-import PantryItem from "../components/PantryItem";
+import PantryItem from "../components/pantryItem";
 
 class Pantry extends React.Component {
-    componentDidMount() {
-        this.props.loadPantry(this.props.userId);
-    }
+  componentDidMount() {
+    this.props.loadPantry(this.props.userId);
+  }
 
-    togglePantryItem(userId, ingredientName) {
-        const newIngredients = this.props.ingredients;
-        for (const ingredient of newIngredients) {
-            if (ingredient.name === ingredientName) {
-                ingredient.amount = (ingredient.amount + 1) % 2;
-            }
-        }
-        API.updateUser(userId, { ingredients: newIngredients });
-        this.props.loadPantry();
+  togglePantryItem(userId, ingredientName) {
+    const newIngredients = this.props.ingredients;
+    for (const ingredient of newIngredients) {
+      if (ingredient.name === ingredientName) {
+        ingredient.amount = (ingredient.amount + 1) % 2;
+      }
     }
+    API.updateUser(userId, { ingredients: newIngredients });
+    this.props.loadPantry();
+  }
 
-    render() {
-        return (
-            <div className="tile is-parent is-8">
-                <article className="tile is-child notification is-bold is-success">
-                    <p className="title">My Pantry:</p>
-                    <p className="subtitle has-text-centered">
-                        {this.props.ingredients.map(ingredient => (
-                            <PantryItem
-                                name={ingredient.name}
-                                key={ingredient.name}
-                                amount={ingredient.amount}
-                                onClick={() =>
-                                    this.togglePantryItem(
-                                        this.props.userId,
-                                        ingredient.name
-                                    )
-                                }
-                            />
-                        ))}
-                    </p>
-                </article>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="tile is-parent is-8">
+        <article className="tile is-child notification is-bold is-success">
+          <p className="title">My Pantry:</p>
+          <p className="subtitle has-text-centered">
+            {this.props.ingredients.map(ingredient => (
+              <PantryItem
+                name={ingredient.name}
+                key={ingredient.name}
+                amount={ingredient.amount}
+                onClick={() =>
+                  this.togglePantryItem(this.props.userId, ingredient.name)
+                }
+              />
+            ))}
+          </p>
+        </article>
+      </div>
+    );
+  }
 }
 
 export default Pantry;
