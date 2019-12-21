b34e8a6bdf38f507b9313054a3120233d36b6c73
Sean Lim <lim_s@outlook.com>
2019-12-21 11:02:30 -0600

    react router

--- client/src/App.js
+++ client/src/App.js
@@ -6,47 +6,51 @@ import Header from "./components/header/header";
 import Footer from "./components/footer/footer";
 import Pantry from "./pages/Pantry";
 import Shoplist from "./pages/Shoplist";
-import Container from "./pages/Container";
 import API from "./utils/API";
+import Recipe from "./pages/Recipes";
 
 class App extends React.Component {
-    state = {
-        userId: "5dfafab3a612c2884b4bd0bd",
-        ingredients: []
-    };
+  state = {
+    userId: "5dfafab3a612c2884b4bd0bd",
+    ingredients: []
+  };
 
-    componentDidMount() {
-        this.loadPantry();
-    }
+  componentDidMount() {
+    this.loadPantry();
+  }
 
-    loadPantry = id => {
-        API.getUser(id).then(user => {
-            this.setState({ ingredients: user.data.ingredients });
-        });
-        this.forceUpdate();
-    };
+  loadPantry = id => {
+    API.getUser(id).then(user => {
+      this.setState({ ingredients: user.data.ingredients });
+    });
+    this.forceUpdate();
+  };
 
-    render() {
-        return (
-            <>
-                <Header />
-                {/* <Container> */}
-                <Pantry
-                    ingredients={this.state.ingredients}
-                    loadPantry={this.loadPantry}
-                    userId={this.state.userId}
-                />
-                <Shoplist
-                    ingredients={this.state.ingredients}
-                    loadPantry={this.loadPantry}
-                    userId={this.state.userId}
-                />
-
-                {/* </Container> */}
-                <Footer />
-            </>
-        );
-    }
+  render() {
+    return (
+      <Router>
+        <Header />
+        <Switch>
+          <Route path="/pantry">
+            <Pantry
+              ingredients={this.state.ingredients}
+              loadPantry={this.loadPantry}
+              userId={this.state.userId}
+            />
+            <Shoplist
+              ingredients={this.state.ingredients}
+              loadPantry={this.loadPantry}
+              userId={this.state.userId}
+            />
+            <Route path="/">
+              <Recipe />
+            </Route>
+          </Route>
+        </Switch>
+        <Footer />
+      </Router>
+    );
+  }
 }
 
 export default App;
