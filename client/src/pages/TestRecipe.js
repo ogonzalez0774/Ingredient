55450a22f8f126e1237ba181e85597f5a80afc2f
Oscar <ogonzalez0774@gmail.com>
2019-12-21 13:45:22 -0600

    added render for recipes

--- client/src/pages/TestRecipe.js
+++ client/src/pages/TestRecipe.js
@@ -3,53 +3,70 @@ import API from "../utils/API";
 import Recipe from "../components/Recipe";
 
 class TestRecipe extends React.Component {
-    state = {
-        recipes: [
-            {
-                name: "Chicken Alfredo",
-                ingredients: [
-                    "Kosher salt",
-                    "freshly ground black pepper",
-                    "fettuccine",
-                    "Olive oil",
-                    "chicken breast",
-                    "unsalted butter",
-                    "heavy cream",
-                    "freshly grated nutmeg",
-                    "Parmigiano-Reggiano"
-                ]
-            },
-            {
-                name: "BLT",
-                ingredients: ["bacon", "lettuce", "tomato", "bread", "mayo"]
-            },
-            {
-                name: "Cheese Burger",
-                ingredients: [
-                    "ground beef",
-                    "lettuce",
-                    "tomato",
-                    "bread",
-                    "mayo",
-                    "cheese"
-                ]
-            },
-            {
-                name: "Pizza",
-                ingredients: ["tomato sauce", "cheese", "dough", "pepperoni"]
-            }
+  state = {
+    recipes: [
+      {
+        name: "Chicken Alfredo",
+        ingredients: [
+          "Kosher salt",
+          "freshly ground black pepper",
+          "fettuccine",
+          "Olive oil",
+          "chicken breast",
+          "unsalted butter",
+          "heavy cream",
+          "freshly grated nutmeg",
+          "Parmigiano-Reggiano"
         ]
-    };
+      },
+      {
+        name: "BLT",
+        ingredients: ["bacon", "lettuce", "tomato", "bread", "mayo"]
+      },
+      {
+        name: "Cheese Burger",
+        ingredients: [
+          "ground beef",
+          "lettuce",
+          "tomato",
+          "bread",
+          "mayo",
+          "cheese"
+        ]
+      },
+      {
+        name: "Pizza",
+        ingredients: ["tomato sauce", "cheese", "dough", "pepperoni"]
+      }
+    ]
+  };
 
-    componentDidMount() {}
+  componentDidMount() {}
 
-    toggleIngredient() {}
+  toggleIngredient() {}
 
-    render() {
-        return this.state.recipes.map(recipe => (
-            <Recipe name={recipe.name} ingredients={recipe.ingredients} />
-        ));
-    }
+  render() {
+    return (
+      <div>
+        {this.state.recipes.map(recipe => (
+          <div className="tile">
+            <div className="tile is-parent">
+              <article className="tile is-child notification is-bold has-background-grey-light">
+                <p className="title has-text-white">{recipe.name}:</p>
+                <p className="subtitle has-text-white">
+                  Ingredients used: &nbsp;
+                  {recipe.ingredients.map(ingredient => (
+                    <span>{ingredient}, </span>
+                  ))}
+                </p>
+                <p className="has-text-white">...Brief overview of recipe...</p>
+              </article>
+            </div>
+          </div>
+        ))}
+      </div>
+    );
+  }
 }
 
 export default TestRecipe;
