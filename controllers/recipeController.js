bb4f258b1fef4d0a7183514204362f4986672434
Oscar <ogonzalez0774@gmail.com>
2020-01-04 13:25:31 -0600

    added regex to ingridient search

--- controllers/recipeController.js
+++ controllers/recipeController.js
@@ -11,8 +11,7 @@ module.exports = {
         { name: { $regex: query, $options: "i" } },
         {
           ingredients: {
-            name: query,
-            amount: 1
+            $elemMatch: { name: { $regex: query, $options: "i" } }
           }
         }
       ]
