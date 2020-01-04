aaa27a9c815595bc6f41698a3b2e5cd6e57cd6ed
Sean Lim <lim_s@outlook.com>
2020-01-04 16:16:27 -0600

    signup creates user on firebase side, need mongo

---
+++ client/src/components/auth/signup.js
@@ -0,0 +1,108 @@
+import React, { Component } from "react";
+// import { Link } from "react-router-dom";
+
+const initialState = {
+  username: "",
+  email: "",
+  passwordOne: "",
+  passwordTwo: "",
+  error: null
+};
+class SignUpForm extends Component {
+  state = { ...initialState };
+
+  onSubmit = event => {
+    event.preventDefault();
+    const { username, email, passwordOne } = this.state;
+    this.props.firebase.signOutUser();
+    this.props.firebase
+      .signUpUser(username, email, passwordOne)
+      .then(authUser => {
+        console.log(authUser);
+        this.setState({ ...initialState });
+      })
+      .catch(error => {
+        this.setState({ error });
+      });
+  };
+  onChange = event => {
+    if (event.target.name == "passwordTwo") {
+    }
+
+    this.setState({ [event.target.name]: event.target.value });
+  };
+
+  render() {
+    const { username, email, passwordOne, passwordTwo, error } = this.state;
+
+    return (
+      <div className="tile is-parent is-8">
+        <div className="tile is-child notification">
+          <form action="" onSubmit={this.onSubmit}>
+            <label htmlFor="username">
+              username:
+              <input
+                className="input is-small"
+                name="username"
+                value={username}
+                onChange={this.onChange}
+                type="text"
+                required
+              />
+            </label>
+            <label htmlFor="email">
+              email:
+              <input
+                className="input is-small"
+                name="email"
+                value={email}
+                onChange={this.onChange}
+                type="email"
+                required
+              />
+            </label>
+            <label htmlFor="passwordOne">
+              password:
+              <input
+                className="input is-small"
+                name="passwordOne"
+                value={passwordOne}
+                onChange={this.onChange}
+                type="password"
+                minLength="6"
+                required
+              />
+            </label>
+            <label htmlFor="passwordTwo">
+              confirm password:
+              <input
+                className="input is-small"
+                name="passwordTwo"
+                value={passwordTwo}
+                onChange={this.onChange}
+                type="password"
+                required
+              />
+            </label>
+
+            <input className="button" type="submit" value="Submit" />
+
+            {error && <p>{error.message}</p>}
+          </form>
+          Already have an account? <a href="#">Log in here</a>
+        </div>
+      </div>
+    );
+  }
+}
+// const SignUpLink = () => (
+//   <p>
+// {
+/* Don't have an account? <Link to={ROUTES.SIGN_UP}>Sign Up</Link> */
+// }
+//   </p>
+// );
+
+export default SignUpForm;
+// export default SignUpPage;
+// export { SignUpForm, SignUpLink };
