c6b914da25819aac26a4eaaafd3a424a9ff0f134
Todd Tracy <trvcytg@gmail.com>
2020-01-06 19:06:47 -0600

    Shoplist.js: Put Queue and Shopping list side-by-side

--- client/src/pages/Shoplist.js
+++ client/src/pages/Shoplist.js
@@ -32,74 +32,79 @@ class Shoplist extends React.Component {
   render() {
     return (
       // YELLOW BOX
-      <div className="tile is-parent is-8">
-        <article className="tile is-child notification is-bold is-warning">
-          {/* QUEUE */}
-          <div>
-            <p className="subtitle">Queued Recipes:</p>
-            <ul>
-              {this.props.queuedRecipes.map(recipe => {
-                return (
-                  <li key={recipe.name}>
-                    <button
-                      className="button is-warning is-light is-outlined is-inverted"
-                      onClick={() => {
-                        const mealSchedule = this.props.queuedRecipes;
-                        for (const scheduledRecipe of mealSchedule) {
-                          if (scheduledRecipe.name === recipe.name) {
-                            scheduledRecipe.toggleShow = !scheduledRecipe.toggleShow;
+      <div className="columns">
+        <div className="column tile is-parent is-two-thirds">
+          <article className="tile is-child notification is-bold is-warning">
+            {/* QUEUE */}
+            <div>
+              <p className="subtitle">Queued Recipes:</p>
+              <ul>
+                {this.props.queuedRecipes.map(recipe => {
+                  return (
+                    <li key={recipe.name}>
+                      <button
+                        className="button is-warning is-light is-outlined is-inverted"
+                        onClick={() => {
+                          const mealSchedule = this.props.queuedRecipes;
+                          for (const scheduledRecipe of mealSchedule) {
+                            if (scheduledRecipe.name === recipe.name) {
+                              scheduledRecipe.toggleShow = !scheduledRecipe.toggleShow;
+                            }
                           }
-                        }
-                        this.setState({
-                          schedule: mealSchedule
-                        });
-                      }}
-                    >
-                      {recipe.name}
-                    </button>
-                    {recipe.toggleShow ? (
-                      <Recipe
-                        name={recipe.name}
-                        ingredients={recipe.ingredients}
-                      />
-                    ) : (
-                      <div className="hidden"></div>
-                    )}
-                  </li>
-                );
-              })}
-            </ul>
-          </div>
+                          this.setState({
+                            schedule: mealSchedule
+                          });
+                        }}
+                      >
+                        {recipe.name}
+                      </button>
+                      {recipe.toggleShow ? (
+                        <Recipe
+                          name={recipe.name}
+                          ingredients={recipe.ingredients}
+                        />
+                      ) : (
+                        <div className="hidden"></div>
+                      )}
+                    </li>
+                  );
+                })}
+              </ul>
+            </div>
+          </article>
+        </div>
+        {/* SHOPPING LIST */}
+        <div className="column tile is-parent">
+          <article className="tile is-child notification is-bold is-info">
+            <form onSubmit={this.handleSubmit}>
+              <p className="subtitle">Shopping List</p>
 
-          {/* SHOPPING LIST */}
-          <form onSubmit={this.handleSubmit}>
-            <p className="subtitle">Shopping List</p>
+              {/* {console.log(this.props.shoppingList)} */}
+              {Object.keys(this.props.shoppingList).map(title => (
+                <ListItem
+                  name={title}
+                  // unsure how to satisfy unique key condition. this isn't it
+                  key={"list" + title}
+                  value={this.props.shoppingList[title]}
+                ></ListItem>
+              ))}
 
-            {/* {console.log(this.props.shoppingList)} */}
-            {Object.keys(this.props.shoppingList).map(title => (
-              <ListItem
-                name={title}
-                // unsure how to satisfy unique key condition. this isn't it
-                key={"list" + title}
-                value={this.props.shoppingList[title]}
-              ></ListItem>
-            ))}
+              <input
+                type="submit"
+                name="addSelected"
+                value="Add Selected"
+                className="button is-info is-light is-outlined is-inverted"
+              ></input>
 
-            <input
-              type="submit"
-              name="addSelected"
-              value="Add Selected"
-              className="button is-warning is-light is-outlined is-inverted"
-            ></input>
-
-            <input
-              type="submit"
-              name="addAll"
-              value="Add All"
-              className="button is-warning is-light is-outlined is-inverted"
-            ></input>
-          </form>
-        </article>
+              <input
+                type="submit"
+                name="addAll"
+                value="Add All"
+                className="button is-info is-light is-outlined is-inverted"
+              ></input>
+            </form>
+          </article>
+        </div>
       </div>
     );
   }
