3d4eb39490c165c865a40f14a0dd5cc169c33417
Todd Tracy <trvcytg@gmail.com>
2020-01-08 13:36:31 -0600

    openSignUp.js: changed button format

--- client/src/components/authbuttons/openSignUp.js
+++ client/src/components/authbuttons/openSignUp.js
@@ -4,7 +4,10 @@ class OpenSignup extends React.Component {
   render() {
     return (
       <div className="navbar-item">
-        <button className="button" onClick={this.props.openSignup}>
+        <button
+          className="button is-small is-info is-outlined is-inverted"
+          onClick={this.props.openSignup}
+        >
           Sign Up
         </button>
       </div>
