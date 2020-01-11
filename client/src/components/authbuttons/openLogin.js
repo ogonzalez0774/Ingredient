04988bdf75dbe0076cc8c51d05679cfcc739bb05
Todd Tracy <trvcytg@gmail.com>
2020-01-11 14:40:22 -0600

    used bulma is-pulled-left

--- client/src/components/authbuttons/openLogin.js
+++ client/src/components/authbuttons/openLogin.js
@@ -3,12 +3,14 @@ import React from "react";
 class OpenLogin extends React.Component {
   render() {
     return (
-      <button
-        className="button is-small is-info is-outlined is-inverted"
-        onClick={this.props.openLogin}
-      >
-        Log In
-      </button>
+      <div className="navbar-item is-pulled-left">
+        <button
+          className="button is-small is-info is-outlined is-inverted"
+          onClick={this.props.openLogin}
+        >
+          Log In
+        </button>
+      </div>
     );
   }
 }
