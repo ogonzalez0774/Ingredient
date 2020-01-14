f80651aba686dc024cdcbf6cc4f6166e09307583
Kelly Jiang <kllyjiang@gmail.com>
2020-01-13 20:19:23 -0600

    Adding units to pantryItem

--- client/src/components/pantryItem.js
+++ client/src/components/pantryItem.js
@@ -1,18 +1,23 @@
 import React from "react";
+const units = require("../units.json");
 
 class PantryItem extends React.Component {
-  render() {
-    if (this.props.quantity > 0) {
-      return (
-        <button
-          className="button is-success is-light is-outlined is-inverted"
-          onDoubleClick={this.props.onDoubleClick}
-        >
-          {this.props.name}, {this.props.quantity}
-        </button>
-      );
-    } else return null;
-  }
+    render() {
+        if (this.props.quantity > 0) {
+            return (
+                <button
+                    className="button is-success is-light is-outlined is-inverted"
+                    onDoubleClick={this.props.onDoubleClick}
+                >
+                    {this.props.name}, {this.props.quantity}{" "}
+                    {units[this.props.name].unit}
+                    {this.props.quantity > 1 && units[this.props.name].unit
+                        ? "s"
+                        : ""}
+                </button>
+            );
+        } else return null;
+    }
 }
 
 export default PantryItem;
