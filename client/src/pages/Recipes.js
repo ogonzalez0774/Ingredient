068807ef709bad03c6020df9a6dad7984390fc88
Todd Tracy <trvcytg@gmail.com>
2020-01-13 19:48:12 -0600

    Recipes.js: Added spacing for better mobile view

--- client/src/pages/Recipes.js
+++ client/src/pages/Recipes.js
@@ -24,48 +24,52 @@ class Recipes extends React.Component {
 
   render() {
     return (
-      <div className="tile is-ancestor">
-        <div className="container">
-          <div className="box has-background-white">
-            <div className="tile">
-              <div className="tile is-parent">
-                <form
-                  onSubmit={this.handleFormSubmit}
-                  className="tile is-child notification is-bold has-background-grey-light"
-                >
-                  <p className="title has-text-white">Search:</p>
-                  <div className="field">
-                    <div className="control">
-                      <input
-                        className="input is-success"
-                        type="text"
-                        name="search"
-                        placeholder="What're you hungry for?"
-                        onChange={this.handleInputChange}
-                      />
-                      <input
-                        className="button is-success"
-                        readOnly
-                        type="submit"
-                        value="Search"
-                      ></input>
+      <>
+        <div className="tile is-ancestor">
+          <div className="container">
+            <div className="box has-background-white">
+              <div className="tile">
+                <div className="tile is-parent">
+                  <form
+                    onSubmit={this.handleFormSubmit}
+                    className="tile is-child notification is-bold has-background-grey-light"
+                  >
+                    <p className="title has-text-white">Search:</p>
+                    <div className="field">
+                      <div className="control">
+                        <input
+                          className="input is-success"
+                          type="text"
+                          name="search"
+                          placeholder="What're you hungry for?"
+                          onChange={this.handleInputChange}
+                        />
+                        <input
+                          className="button is-success"
+                          readOnly
+                          type="submit"
+                          value="Search"
+                        ></input>
+                      </div>
                     </div>
-                  </div>
-                </form>
+                  </form>
+                </div>
               </div>
-            </div>
 
-            {this.state.result.map(recipe => (
-              <Recipe
-                name={recipe.name}
-                ingredients={recipe.ingredients}
-                addToQueue={this.props.addToQueue}
-                removeFromQueue={this.props.removeFromQueue}
-              />
-            ))}
+              {this.state.result.map(recipe => (
+                <Recipe
+                  name={recipe.name}
+                  ingredients={recipe.ingredients}
+                  addToQueue={this.props.addToQueue}
+                  removeFromQueue={this.props.removeFromQueue}
+                />
+              ))}
+            </div>
           </div>
         </div>
-      </div>
+        <br />
+        <br />
+      </>
     );
   }
 }
